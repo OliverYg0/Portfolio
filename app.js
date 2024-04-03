fd25c8c3b8b1d94f686687608437380668ecd3ae
OliverYg0 <oliverzygo@duck.com>
2024-04-04 00:34:46 +0100

    installed pug and got a index page working.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
 
 app.listen(port, () => {
     console.log('Express app is listening on port', port)
