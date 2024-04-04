4c8bfab0f941ce9351585cccbbed7f2acd08c55f
OliverYg0 <oliverzygo@duck.com>
2024-04-04 10:12:08 +0100

    Favicons working on edge but doesn't seem to be working on brave. Everything else is up and running.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'pug')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'icons', 'favicon.ico')))
 
 app.use((req, res, next) => {
     res.locals.title = 'Oliver Zygo'
