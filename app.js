ac76470c299d31218cdc48665e14eed44dca3c49
OliverYg0 <oliverzygo@duck.com>
2024-04-04 01:05:39 +0100

    Setting up public folder to add static files in such as styles.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'pug')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    res.locals.title = 'Oliver Zygo'
+    next()
+})
+
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { page: 'Home', message: 'Hello there!' })
   })
 
 app.listen(port, () => {
-    console.log('Express app is listening on port', port)
+    console.log('App is listening on port', port)
 })
