d889b0fa102976fe094150e16dedce32f0a0a3ae
OliverYg0 <oliverzygo@duck.com>
2024-04-06 23:56:41 +0100

    Removed the header-scripts.js file and put it's contents in the general js file. Removed the desktop css file as I no longer think it is nessasery to have more than the mobile and regular stylesheets. Got the mobile version of the header worker almost fully.

--- public/scripts/general-scripts.js
+++ public/scripts/general-scripts.js
@@ -1,1 +1,23 @@
-// Pause scrolling function here
+function pauseScrolling() {
+    document.body.classList.toggle('scrolling-paused')
+}
+
+const navToggle = document.querySelector('.nav-toggle')
+const navItems = document.querySelector('.nav-items')
+const navLinks = document.querySelectorAll('.navLinks')
+
+function toggleNav() {
+    navItems.classList.toggle('nav-active')
+    navToggle.classList.toggle('nav-active')
+}
+
+navToggle.addEventListener('click', () => {
+    pauseScrolling()
+    toggleNav()
+})
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        toggleNav()
+    })
+})
