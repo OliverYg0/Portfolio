b9df48aa22a04ccee2ac31b16f40e1c495d355f4
Oliver Zygo <oliverzygo@duck.com>
2026-04-02 20:12:16 +0100

    Added html and css.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const app = express()
+const port = 3000
 
-const app = express();
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'icons', 'favicon.ico')))
+
+app.use((req, res, next) => {
+    res.locals.title = 'Oliver Zygo'
+    next()
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js servers');
-});
+    res.render('index', { page: 'Home'})
+})
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on port: ${server.address().port}`);
-});
+app.get('/contact', (req, res) => {
+    res.render('contact', {page: 'Contact'})
+})
+
+const server = app.listen(process.env.PORT || port, () => {
+  console.log(`Server started on port: ${server.address().port}`)
+})
---
+++ src/public/scripts/scripts.js
@@ -0,0 +1,24 @@
+function pauseScrolling() {
+    document.body.classList.toggle('scrolling-paused')
+}
+
+const navToggle = document.querySelector('.nav-toggle')
+const navItems = document.querySelector('.nav-items')
+const navLinks = document.querySelectorAll('.nav-link')
+
+function toggleNav() {
+    navItems.classList.toggle('nav-active')
+    navToggle.classList.toggle('nav-active')
+}
+
+navToggle.addEventListener('click', () => {
+    pauseScrolling()
+    toggleNav()
+})
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        toggleNav()
+        navLink.classList.add('active')
+    })
+})
